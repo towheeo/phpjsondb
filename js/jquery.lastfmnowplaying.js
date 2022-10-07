5ddd6ddf435dccc953b1a30ebfe141d4e2878dc9
towheeo <112147021+towheeo@users.noreply.github.com>
2022-10-07 20:06:14 +0000

    Update emotes.json

---
+++ js/jquery.lastfmnowplaying.js
@@ -0,0 +1,15 @@
+/**
+ * Last.fm Now Playing
+ * v0.1.1
+ * Author: Mike Mitchell <@innernets> | DevTeam Inc <http://devteaminc.co/>
+ * Licensed under the MIT license
+ */
+
+(function ( $, window, document ) {
+
+	'use strict';
+
+	var pluginName = 'lastfmNowPlaying';
+	var defaults = {};
+
+	function Plugin
